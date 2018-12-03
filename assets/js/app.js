0d10aec19141caec364e2357e7755c5293b6fc76
qlebris <quentin.lebris@gmail.com>
2018-12-03 17:44:44 +0100

    finalisation quete webpack

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,9 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+require('../css/app.scss');
+require('jquery');
+require('bootstrap');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
